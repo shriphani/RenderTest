6168a25dae5dd438ef8d2f18df7d5995273d91d8
Shriphani Palakodety <shriphanip@gmail.com>
2016-04-13 18:26:05 -0700

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+/**
+ * Created by shriphani on 4/13/16.
+ */
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import com.machinepublishers.jbrowserdriver.Timezone;
+import com.machinepublishers.jbrowserdriver.JBrowserDriver;
+import com.machinepublishers.jbrowserdriver.Settings;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        // You can optionally pass a Settings object here,
+        // constructed using Settings.Builder
+        JBrowserDriver driver = new JBrowserDriver(Settings.builder().
+                timezone(Timezone.AMERICA_NEWYORK).build());
+
+        // This will block for the page load and any
+        // associated AJAX requests
+        driver.get("http://blog.shriphani.com");
+
+        // You can get status code unlike other Selenium drivers.
+        // It blocks for AJAX requests and page loads after clicks
+        // and keyboard events.
+        System.out.println(driver.getStatusCode());
+
+        // Returns the page source in its current state, including
+        // any DOM updates that occurred after page load
+        System.out.println(driver.getPageSource());
+
+        File f = driver.getScreenshotAs(OutputType.FILE);
+
+        FileUtils.copyFile(f, new File("screenshot.png"));
+
+        // Close the browser. Allows this thread to terminate.
+        driver.quit();
+    }
+}
